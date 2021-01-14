27de5afe38afe929744e4662771f29e4de18810d
Gabriel Vieira <gabriel.oliveira@yandeh.com.br>
2021-01-14 08:41:51 -0300

    parametro nome adicionado

--- src/main/java/com/example/demo/BitrixController.java
+++ src/main/java/com/example/demo/BitrixController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/teste")
 public class BitrixController {
@@ -10,9 +12,10 @@ public class BitrixController {
     public ResponseEntity teste(){
         return ResponseEntity.ok("teste");
     }
-    @PostMapping("/{idDeal}")
-    public ResponseEntity webhookSaida(@PathVariable Integer idDeal,@RequestBody String json){
+    @PostMapping("/{idDeal}/{nome}")
+    public ResponseEntity webhookSaida(@PathVariable Integer idDeal,@PathVariable String nome,@RequestBody String json){
         System.out.println("idDeal: " + idDeal);
+        System.out.println("nome: " + nome);
         System.out.println("Corpo: " + json);
         return ResponseEntity.ok(idDeal);
     }
